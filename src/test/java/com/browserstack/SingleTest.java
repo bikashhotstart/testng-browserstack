b83fee99c3cc17beec54371f9b4f505765b9a336
Ramkrishna Das <ramu_k06@yahoo.co.in>
2021-06-29 16:48:56 +0530

    Fluent Wait added

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -1,6 +1,8 @@
 package com.browserstack;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
